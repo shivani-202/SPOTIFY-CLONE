4627aa9dedb6426364ac70802b99461a9fa0bc30
shivani sharma <vanis131203@gmail.com>
2023-10-26 13:20:16 +0530

    added cover photo functionality

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-console.log("Welcome to spotify")
-//initialise the variables
+console.log("Welcome to Spotify");
+
+// Initialize the variables
 let songIndex = 0;
-let audioElement = new Audio('track1.mp3');
+let audioElement = new Audio();
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
@@ -14,82 +15,66 @@ let songs = [
     { songName: "TRACK 4", filepath: "track4.mp3", coverPath: "images/c4.jpeg" },
     { songName: "TRACK 5", filepath: "track5.mp3", coverPath: "images/c5.jpeg" }
 ];
- //object 
-//
-// songItems.forEach((element, i)=>{
-//     // console.log(element, i);
-//     element.getElementsByTagName("img")[0].src = songs[i].coverPath;
-//     element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
-// }) 
+let trackNames = [
+    "TRACK 1",
+    "TRACK 2",
+    "TRACK 3",
+    "TRACK 4",
+    "TRACK 5"
+];
 
-//handle play and pause
-masterPlay.addEventListener('click',()=>{
-    if(audioElement.paused||audioElement.currentTime<=0)//i wrote function pause
-    {
-        audioElement.play();
-        masterPlay.classList.remove('fa-circle-play');
-        masterPlay.classList.add('fa-circle-pause');
-        gif.style.opacity =1;
-    }
-    else
-    {
-        audioElement.pause();
-        masterPlay.classList.add('fa-circle-play');
-        masterPlay.classList.remove('fa-circle-pause');
-        gif.style.opacity =0;
-    }
-})
 
-//listen to events
-audioElement.addEventListener('timeupdate', ()=>{
-    // console.log('timeupdate');
-    //update seekbar
-    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
-    // console.log(progress);
-    myProgressBar.value = progress;
-})
-//change
-myProgressBar.addEventListener('change', () =>{
-    audioElement.currentTime= (myProgressBar.value *audioElement.duration)/100;//seek audio
-})
-const makeAllPlays =()=>{
-    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-        element.classList.add("fa-circle-play");
-        element.classList.remove('fa-circle-pause');
-    })
-}
-
-Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-    element.addEventListener('click',(e)=>{
-        makeAllPlays(); 
-        songIndex = parseInt(e.target.id);
-        e.target.classList.remove('fa-circle-play');
-        e.target.classList.add('fa-circle-pause');
-        audioElement.src = '${songIndex+1}.mp3';
-        audioElement.currentTime = 0;
-        audioElement.play();
-        masterPlay.classList.remove('fa-circle-pause');
-        masterPlay.classList.add('fa-circle-pause');
-    } )
-})
-
-document.getElementById('next').addEventListener('click', ()=>{
-    if(songIndex > 5)
-    songIndex = 0;
-    else
-    songIndex +=1;
-})
+// Function to play a song
 const playSong = () => {
     audioElement.src = songs[songIndex].filepath;
     audioElement.currentTime = 0;
     audioElement.play();
     masterPlay.classList.remove('fa-circle-play');
     masterPlay.classList.add('fa-circle-pause');
+    gif.style.opacity = 1;
+    document.getElementById('gif').setAttribute('src', songs[songIndex].coverPath);
+    // document.querySelector('.songInfo').textContent = songs[songIndex].songName;
+    songTitle.textContent = trackNames[songIndex];
 };
+
+// Handle play and pause
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        playSong();
+    } else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity = 0;
+    }
+});
+
+// Play a song by clicking the song item
+songItems.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        songIndex = index;
+        playSong();
+    });
+});
+
+// Update the progress bar as the song plays
+audioElement.addEventListener('timeupdate', () => {
+    const progress = (audioElement.currentTime / audioElement.duration) * 100;
+    myProgressBar.value = progress;
+});
+
+// Seek to a specific part of the song when the progress bar is changed
+myProgressBar.addEventListener('input', () => {
+    const seekTime = (myProgressBar.value / 100) * audioElement.duration;
+    audioElement.currentTime = seekTime;
+});
+
+// Previous and Next button functionality
 document.getElementById('next').addEventListener('click', () => {
     songIndex = (songIndex + 1) % songs.length;
     playSong();
 });
+
 document.getElementById('prev').addEventListener('click', () => {
     songIndex = (songIndex - 1 + songs.length) % songs.length;
     playSong();
